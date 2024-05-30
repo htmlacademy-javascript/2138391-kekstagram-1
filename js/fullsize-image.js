08f63fe29da0b9b1becd86fabb6fbd559c7ff483
PkMsk <pk693@icloud.com>
2024-05-30 23:41:13 +0300

    Создает шаблон для комментария и удаляет существующие из разметки

--- js/fullsize-image.js
+++ js/fullsize-image.js
@@ -4,8 +4,9 @@ import './midget.js';
 const fullsizeModal = document.querySelector('.big-picture');
 const previewImageParent = document.querySelector('.pictures');
 const fullsizeModalCloseBtn = document.querySelector('.big-picture__cancel');
+const commentsList = fullsizeModal.querySelector('.social__comments');
+const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 
-const imagesData = createImagesData();
 
 previewImageParent.addEventListener('click', (evt) => {
   if (evt.target.matches('.picture__img')) {
@@ -15,18 +16,19 @@ previewImageParent.addEventListener('click', (evt) => {
     const pictureLink = evt.target.closest('.picture');
     fullsizeModal.querySelector('.likes-count').textContent = pictureLink.querySelector('.picture__likes').textContent;
     fullsizeModal.querySelector('.comments-count').textContent = pictureLink.querySelector('.picture__comments').textContent;
-    const commentsList = fullsizeModal.querySelector('.social__comments');
     const commentData = createImageData();
     commentData.comments.forEach((comment) => {
-      const commentItem = fullsizeModal.querySelector('.social__comment').cloneNode(true);
+      const commentItem = commentTemplate.cloneNode(true);
       commentItem.querySelector('img').src = comment.avatar;
       commentItem.querySelector('img').alt = comment.name;
       commentItem.querySelector('.social__text').textContent = comment.message;
       commentsList.append(commentItem);
     });
+    fullsizeModal.querySelector('.social__caption').textContent = commentData.description;
   }
 });
 
 fullsizeModalCloseBtn.addEventListener('click', () => {
   fullsizeModal.classList.add('hidden');
+  commentsList.innerHTML = '';
 })
