504d4228a71e8cdcd1049dca581b2b7628596851
PkMsk <pk693@icloud.com>
2024-06-01 15:44:51 +0300

    Создает функцию создания комментария

--- js/fullsize-image.js
+++ js/fullsize-image.js
@@ -4,11 +4,14 @@ import {isEscapeKey} from './util.js';
 //Модальное окно полноэкранного изображения
 const fullsizeImgModal = document.querySelector('.big-picture');
 
-// Список комментариев к изображению
+//Список комментариев к изображению
 const commentsList = fullsizeImgModal.querySelector('.social__comments');
 
 //Шаблон для комментария
 const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+
+//Фрагмент для хранения создаваемых комментариев
+const commentsFragment = document.createDocumentFragment();
 
 //Callback функция для document при наступлении события 'keydown', нажатии клавиши Esc
 const onDocumentKeydownEsc = (evt) => {
@@ -16,7 +19,18 @@ const onDocumentKeydownEsc = (evt) => {
     evt.preventDefault();
     closeFullsizeImgModal();
   }
-}
+};
+
+//Функция создания комментария
+const createComment = (imageData) => {
+  imageData.comments.forEach((comment) => {
+    const commentItem = commentTemplate.cloneNode(true);
+    commentItem.querySelector('img').src = comment.avatar;
+    commentItem.querySelector('img').alt = comment.name;
+    commentItem.querySelector('.social__text').textContent = comment.message;
+    commentsFragment.append(commentItem);
+  });
+};
 
 function openFullsizeImgModal (evt) {
   if (evt.target.matches('.picture__img')) {
@@ -27,13 +41,8 @@ function openFullsizeImgModal (evt) {
     fullsizeImgModal.querySelector('.likes-count').textContent = imageLink.querySelector('.picture__likes').textContent;
     fullsizeImgModal.querySelector('.comments-count').textContent = imageLink.querySelector('.picture__comments').textContent;
     const imageData = createImageData();
-    imageData.comments.forEach((comment) => {
-      const commentItem = commentTemplate.cloneNode(true);
-      commentItem.querySelector('img').src = comment.avatar;
-      commentItem.querySelector('img').alt = comment.name;
-      commentItem.querySelector('.social__text').textContent = comment.message;
-      commentsList.append(commentItem);
-    });
+    createComment(imageData);
+    commentsList.append(commentsFragment);
     fullsizeImgModal.querySelector('.social__caption').textContent = imageData.description;
     fullsizeImgModal.querySelector('.social__comment-count').classList.add('hidden');
     fullsizeImgModal.querySelector('.comments-loader').classList.add('hidden');
