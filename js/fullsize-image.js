6f7698e350855cd59cd6a5722186a2fd13ddde61
PkMsk <pk693@icloud.com>
2024-06-01 15:28:51 +0300

    Меняет название переменной для модального полноэкранного режима

--- js/fullsize-image.js
+++ js/fullsize-image.js
@@ -2,10 +2,10 @@ import {createImageData} from './data.js';
 import {isEscapeKey} from './util.js';
 
 //Модальное окно полноэкранного изображения
-const fullsizeModal = document.querySelector('.big-picture');
+const fullsizeImgModal = document.querySelector('.big-picture');
 
 // Список комментариев к изображению
-const commentsList = fullsizeModal.querySelector('.social__comments');
+const commentsList = fullsizeImgModal.querySelector('.social__comments');
 
 //Шаблон для комментария
 const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
@@ -20,37 +20,33 @@ const onDocumentKeydownEsc = (evt) => {
 
 function openFullsizeImgModal (evt) {
   if (evt.target.matches('.picture__img')) {
-    fullsizeModal.classList.remove('hidden');
-    const fullsizeImage = document.querySelector('.big-picture__img');
+    fullsizeImgModal.classList.remove('hidden');
+    const fullsizeImage = fullsizeImgModal.querySelector('.big-picture__img');
     fullsizeImage.querySelector('img').src = evt.target.src;
-    const pictureLink = evt.target.closest('.picture');
-    fullsizeModal.querySelector('.likes-count').textContent = pictureLink.querySelector('.picture__likes').textContent;
-    fullsizeModal.querySelector('.comments-count').textContent = pictureLink.querySelector('.picture__comments').textContent;
-    const commentData = createImageData();
-    commentData.comments.forEach((comment) => {
+    const imageLink = evt.target.closest('.picture');
+    fullsizeImgModal.querySelector('.likes-count').textContent = imageLink.querySelector('.picture__likes').textContent;
+    fullsizeImgModal.querySelector('.comments-count').textContent = imageLink.querySelector('.picture__comments').textContent;
+    const imageData = createImageData();
+    imageData.comments.forEach((comment) => {
       const commentItem = commentTemplate.cloneNode(true);
       commentItem.querySelector('img').src = comment.avatar;
       commentItem.querySelector('img').alt = comment.name;
       commentItem.querySelector('.social__text').textContent = comment.message;
       commentsList.append(commentItem);
     });
-    fullsizeModal.querySelector('.social__caption').textContent = commentData.description;
-    fullsizeModal.querySelector('.social__comment-count').classList.add('hidden');
-    fullsizeModal.querySelector('.comments-loader').classList.add('hidden');
+    fullsizeImgModal.querySelector('.social__caption').textContent = imageData.description;
+    fullsizeImgModal.querySelector('.social__comment-count').classList.add('hidden');
+    fullsizeImgModal.querySelector('.comments-loader').classList.add('hidden');
     document.querySelector('body').classList.add('modal-open');
     document.addEventListener('keydown', onDocumentKeydownEsc);
   }
 }
 
-
-
 function closeFullsizeImgModal () {
-  fullsizeModal.classList.add('hidden');
+  fullsizeImgModal.classList.add('hidden');
   commentsList.innerHTML = '';
   document.querySelector('body').classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydownEsc);
 }
-
-
 
 export{openFullsizeImgModal, closeFullsizeImgModal};
