793409bb535b07d3c7d3ca6387d70dc9055e4a2c
PkMsk <pk693@icloud.com>
2024-06-04 23:51:57 +0300

    Добавляет загрузку комментариев в модалке полноразмерного изображения

--- js/fullsize-img-modal.js
+++ js/fullsize-img-modal.js
@@ -21,6 +21,7 @@ const onDocumentKeydownEsc = (evt) => {
   }
 };
 
+
 //Функция создания комментария
 const createComment = (imageData) => {
   imageData.comments.forEach((comment) => {
@@ -41,8 +42,51 @@ function openFullsizeImgModal (evt) {
     fullsizeImgModal.querySelector('.likes-count').textContent = imageLink.querySelector('.picture__likes').textContent;
     fullsizeImgModal.querySelector('.comments-count').textContent = imageLink.querySelector('.picture__comments').textContent;
     const imageData = createImageData();
-    createComment(imageData);
+    const loadedCommentsQty = fullsizeImgModal.querySelector('.comments-loaded');
+    loadedCommentsQty.textContent = 0;
+    // Загрузка первых пяти комментариев или менее, если в массиве меньше 5 элементов
+    if (imageData.comments.length > 5) {
+      for (let i = 0; i < 5; i++) {
+        const commentItem = commentTemplate.cloneNode(true);
+        commentItem.querySelector('img').src = imageData.comments[i].avatar;
+        commentItem.querySelector('img').alt = imageData.comments[i].name;
+        commentItem.querySelector('.social__text').textContent = imageData.comments[i].message;
+        loadedCommentsQty.textContent++;
+        commentsFragment.append(commentItem);
+      }
+      imageData.comments.splice(0, 5);
+    } else {
+      createComment(imageData);
+      loadedCommentsQty.textContent = imageData.comments.length;
+      imageData.comments.splice(0, imageData.comments.length);
+    }
     commentsList.append(commentsFragment);
+    //Кнопка "Загрузить еще"
+    const loadCommentsBtn = fullsizeImgModal.querySelector('.social__comments-loader');
+    //Callback функция для события click кнопки "Загрузить еще"
+    const onLoadCommentsBtnClick = () => {
+      if (imageData.comments.length > 5) {
+        for (let i = 0; i < 5; i++) {
+          const commentItem = commentTemplate.cloneNode(true);
+          commentItem.querySelector('img').src = imageData.comments[i].avatar;
+          commentItem.querySelector('img').alt = imageData.comments[i].name;
+          commentItem.querySelector('.social__text').textContent = imageData.comments[i].message;
+          loadedCommentsQty.textContent++;
+          commentsFragment.append(commentItem);
+        }
+        imageData.comments.splice(0, 5);
+      } else {
+        createComment(imageData);
+        loadedCommentsQty.textContent = Number(loadedCommentsQty.textContent) + imageData.comments.length;
+        imageData.comments.splice(0, imageData.comments.length);
+      }
+      commentsList.append(commentsFragment);
+      //Удаляет обработчик событий с кнопки "Загрузить еще" когда все комменты загружены
+      if (imageData.comments.length === 0) {
+        loadCommentsBtn.removeEventListener('click', onLoadCommentsBtnClick);
+      }
+    };
+    loadCommentsBtn.addEventListener('click', onLoadCommentsBtnClick);
     fullsizeImgModal.querySelector('.social__caption').textContent = imageData.description;
     document.querySelector('body').classList.add('modal-open');
     document.addEventListener('keydown', onDocumentKeydownEsc);
