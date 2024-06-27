997e4df10d891dd86996157850421470138c8f2d
PkMsk <pk693@icloud.com>
2024-06-28 00:08:45 +0300

    Добавляет функционал для обработки загружаемого изображения

--- js/img-load-modal.js
+++ js/img-load-modal.js
@@ -12,6 +12,27 @@ const hashtagsField = imgLoadModal.querySelector('.text__hashtags');
 
 const commentField = imgLoadModal.querySelector('.text__description');
 
+const scaleMinusBtn = imgLoadModal.querySelector('.scale__control--smaller');
+
+const scalePlusBtn = imgLoadModal.querySelector('.scale__control--bigger');
+
+const scaleValueField = imgLoadModal.querySelector('.scale__control--value');
+
+const imgUploadPreview = imgLoadModal.querySelector('.img-upload__preview');
+
+const imgEffectLevelContainer = imgLoadModal.querySelector('.img-upload__effect-level');
+
+const imgEffectsContainer = imgLoadModal.querySelector('.effects__list');
+
+const imgBasicClass = imgUploadPreview.className;
+
+const effectNofilter = imgLoadModal.querySelector('#effect-none');
+
+const imgEffectSlider = imgLoadModal.querySelector('.effect-level__slider');
+
+const effectInput = imgLoadModal.querySelector('.effect-level__value');
+
+
 // Регулярное выражение для проверки состава хэштега
 
 const hashtagRegExp = /^#[a-zа-яё0-9]{1,19}$/i;
@@ -29,6 +50,10 @@ function openImgLoadModal () {
   imgEditor.classList.remove('hidden');
   document.querySelector('body').classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydownEsc);
+  scaleValueField.value = '100%';
+  imgUploadPreview.style.transform = 'scale(1)';
+  effectNofilter.checked = true;
+  imgEffectLevelContainer.classList.add('visually-hidden');
 }
 
 //Callback функция для кнопки закрытия модального окна загрузки изображения при наступлении события 'click'
@@ -36,9 +61,13 @@ function closeImgLoadModal () {
   imgEditor.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydownEsc);
+  scaleValueField.readOnly = false;
+  scaleValueField.value = '100%';
   imgLoad.value = '';
   hashtagsField.value = '';
   commentField.value = '';
+  imgUploadPreview.className = imgBasicClass;
+  imgUploadPreview.style.filter = '';
 }
 
 imgLoad.addEventListener('change', () => {
@@ -142,4 +171,154 @@ imgLoadModal.addEventListener('submit', (evt) => {
   if (pristine.validate() !== true) {
     evt.preventDefault();
   }
+});
+
+//Callback функция для кнопки "-" изменения масштаба изображения при наступлении события 'click'
+function scaleDown () {
+  let scaleValueTransformed = parseFloat(scaleValueField.value);
+  if (scaleValueTransformed > 25) {
+    scaleValueTransformed = scaleValueTransformed - 25;
+    scaleValueField.value = `${scaleValueTransformed}%`;
+    imgUploadPreview.style.transform = `scale(${scaleValueField.value})`;
+  }
+}
+
+//Callback функция для кнопки "+" изменения масштаба изображения при наступлении события 'click'
+function scaleUp () {
+  let scaleValueTransformed = parseFloat(scaleValueField.value);
+  if (scaleValueTransformed < 100) {
+    scaleValueTransformed = scaleValueTransformed + 25;
+    scaleValueField.value = `${scaleValueTransformed}%`;
+    imgUploadPreview.style.transform = `scale(${scaleValueField.value})`;
+  }
+}
+
+scaleMinusBtn.addEventListener('click', scaleDown);
+
+scalePlusBtn.addEventListener('click', scaleUp);
+
+//Визуальные фильтры изображения
+
+//Функция применения визуального эффекта на изображение
+function appliesEffect (evt) {
+  if (evt.target.matches('#effect-chrome')) {
+    imgUploadPreview.classList.add('effects__preview--chrome');
+    imgEffectSlider.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 1
+      },
+      start: 1,
+      step: 0.1
+    });
+    imgEffectSlider.noUiSlider.on('update', () => {
+      imgUploadPreview.style.filter = `grayscale(${imgEffectSlider.noUiSlider.get()})`;
+    });
+  } else {
+    imgUploadPreview.classList.remove('effects__preview--chrome');
+  }
+  if (evt.target.matches('#effect-sepia')) {
+    imgUploadPreview.classList.add('effects__preview--sepia');
+    imgEffectSlider.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 1
+      },
+      start: 1,
+      step: 0.1
+    });
+    imgEffectSlider.noUiSlider.on('update', () => {
+      imgUploadPreview.style.filter = `sepia(${imgEffectSlider.noUiSlider.get()})`;
+    });
+  } else {
+    imgUploadPreview.classList.remove('effects__preview--sepia');
+  }
+  if (evt.target.matches('#effect-marvin')) {
+    imgUploadPreview.classList.add('effects__preview--marvin');
+    imgEffectSlider.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 100
+      },
+      start: 100,
+      step: 1
+    });
+    imgEffectSlider.noUiSlider.on('update', () => {
+      imgUploadPreview.style.filter = `invert(${imgEffectSlider.noUiSlider.get()}%)`;
+    });
+  } else {
+    imgUploadPreview.classList.remove('effects__preview--marvin');
+  }
+  if (evt.target.matches('#effect-phobos')) {
+    imgUploadPreview.classList.add('effects__preview--phobos');
+    imgEffectSlider.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 3
+      },
+      start: 3,
+      step: 0.1
+    });
+    imgEffectSlider.noUiSlider.on('update', () => {
+      imgUploadPreview.style.filter = `blur(${imgEffectSlider.noUiSlider.get()}px)`;
+    });
+  } else {
+    imgUploadPreview.classList.remove('effects__preview--phobos');
+  }
+  if (evt.target.matches('#effect-heat')) {
+    imgUploadPreview.classList.add('effects__preview--heat');
+    imgEffectSlider.noUiSlider.updateOptions({
+      range: {
+        min: 1,
+        max: 3
+      },
+      start: 3,
+      step: 0.1
+    });
+    imgEffectSlider.noUiSlider.on('update', () => {
+      imgUploadPreview.style.filter = `brightness(${imgEffectSlider.noUiSlider.get()})`;
+    });
+  } else {
+    imgUploadPreview.classList.remove('effects__preview--heat');
+  }
+  if (evt.target.matches('#effect-none')) {
+    imgEffectLevelContainer.classList.add('visually-hidden');
+    imgUploadPreview.style.filter = '';
+    effectInput.value = '';
+  } else {
+    imgEffectLevelContainer.classList.remove('visually-hidden');
+  }
+}
+
+//Родительский элемент визуальных эффектов, который ловит событие change дочерних элементов и обрабатывает их при помощи обработчика
+imgEffectsContainer.addEventListener('change', (evt) => {
+  appliesEffect(evt);
+});
+
+//Создает слайдер на элементе при помощи noUiSlider
+noUiSlider.create(imgEffectSlider, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: 1,
+  step: 0.1,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      } else {
+        return value.toFixed(1);
+      }
+    },
+    from: function (value) {
+      return parseFloat(value);
+    }
+  }
+});
+
+//добавляет условное событие update при помощи noUISlider на элемент на котором был создан слайдер
+imgEffectSlider.noUiSlider.on('update', () => {
+  effectInput.value = imgEffectSlider.noUiSlider.get();
 });
