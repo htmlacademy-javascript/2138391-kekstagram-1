24e4cd3c031c37bab162721957793e00e59aab9c
Keks <keks@htmlacademy.ru>
2024-08-14 11:05:14 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/midget.js
@@ -0,0 +1,26 @@
+import {openFullsizeImgModal} from './fullsize-img-modal.js';
+
+//Контейнер для миниатюр
+const midgetsContainer = document.querySelector('.pictures');
+
+//Находим в разметке шаблон и в шаблоне находим элемент с нужным классом
+const midgetTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+//Перебираем данные циклом и заполняем ими шаблон
+
+const renderMidgets = (usersImagesData) => {
+  const existingMidgets = midgetsContainer.querySelectorAll('.picture');
+  existingMidgets.forEach((midget) => midget.remove());
+  const midgetsFragment = document.createDocumentFragment();
+  usersImagesData.forEach((userImageData) => {
+    const midget = midgetTemplate.cloneNode(true);
+    midget.querySelector('.picture__img').src = userImageData.url;
+    midget.querySelector('.picture__likes').textContent = userImageData.likes;
+    midget.querySelector('.picture__comments').textContent = userImageData.comments.length;
+    midgetsFragment.append(midget);
+    midget.addEventListener('click', () => openFullsizeImgModal(userImageData));
+  });
+  midgetsContainer.append(midgetsFragment);
+};
+
+export {renderMidgets};
